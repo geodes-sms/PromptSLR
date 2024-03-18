3b908efd35d9a4d3c6e5ddf246b475244d9699fa
Zahra FiyouziSabah <zahrasabah46@gmail.com>
2024-03-18 11:43:08 -0400

    Add text input

--- front/src/components/ConfigurationForm/ConfigurationForm.tsx
+++ front/src/components/ConfigurationForm/ConfigurationForm.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "./styles.scss";
 import DropdownSelector from "../DropdownSelector/DropdownSelector";
+import TextInput from "../TextInput/TextInput";
 
 function ConfigurationForm() {
   const [LLMName, setLLMName] = useState("");
+  const [APIKey, setAPIKey] = useState("");
+  const [temperature, setTemperature] = useState("0.2");
+
   return (
     <div>
       <DropdownSelector
@@ -11,6 +15,16 @@ function ConfigurationForm() {
         dropdownItems={["ChatGPT", "Trainable", "Custom URL"]}
         selectedItem={LLMName}
         setSelectedItem={setLLMName}
+      />
+      <TextInput value={APIKey} setValue={setAPIKey} label="API Key" />
+      <TextInput
+        value={temperature}
+        setValue={setTemperature}
+        label="Temperature"
+        type="number"
+        min={0}
+        max={1}
+        step={0.1}
       />
     </div>
   );
---
+++ front/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
+
+const TextInput = (props: {
+  label: string;
+  value: string;
+  setValue: (item: string) => void;
+  type?: string;
+  min?: number;
+  max?: number;
+  step?: number;
+}) => {
+  return (
+    <div className="text-input">
+      <p className="text-input__label">{props.label}</p>
+      <input
+        value={props.value}
+        type={props.type ? props.type : "text"}
+        onChange={(e) => props.setValue(e.target.value)}
+        className="text-input__field"
+        min={props.min}
+        max={props.max}
+        step={props.step}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
