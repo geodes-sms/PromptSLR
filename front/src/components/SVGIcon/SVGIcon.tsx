35fdfc16a35d53a78b654545ed6ff2f014492102
Zahra FiyouziSabah <zahrasabah46@gmail.com>
2024-03-27 13:58:38 -0400

    Add plus and minus icons

--- front/src/components/SVGIcon/SVGIcon.tsx
+++ front/src/components/SVGIcon/SVGIcon.tsx
@@ -33,76 +33,19 @@ const getPath = (name: string, props: any) => {
         </>
       );
 
-    case "close":
+    case "minus":
       return (
         <>
-          <defs>
-            <style>
-              {`.closeSvgIcon{fill:none;stroke:#919ca9;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px}`}
-            </style>
-          </defs>
-          <g
-            id="prefix__Group_5924"
-            data-name="Group 5924"
-            transform="translate(-498.5 -88.5)"
-          >
-            <path
-              id="prefix__Rectangle_3207"
-              d="M0 0H24V24H0z"
-              data-name="Rectangle 3207"
-              transform="translate(499 89)"
-              style={{ opacity: 0, fill: "none", stroke: "#919ca9" }}
-            />
-            <g
-              id="prefix__Group_4518"
-              data-name="Group 4518"
-              transform="translate(203.177 -580.206)"
-            >
-              <path
-                id="prefix__Line_622"
-                d="M13 0L0 13"
-                className="closeSvgIcon"
-                data-name="Line 622"
-                transform="translate(301.324 674.594)"
-              />
-              <path
-                id="prefix__Line_623"
-                d="M13 13L0 0"
-                className="closeSvgIcon"
-                data-name="Line 623"
-                transform="translate(301.324 674.594)"
-              />
-            </g>
-          </g>
+          <path d="M0 10h24v4h-24z" style={{ fill: props.fill }} />
         </>
       );
-    case "create":
+    case "plus":
       return (
-        <>
-          <defs>
-            <style>
-              {`.createSvgIcon{fill:none;stroke:#29263d;stroke-linecap:round;stroke-linejoin:round}`}
-            </style>
-          </defs>
-          <g id="Group_145420" transform="translate(-522.5 -88.5)">
-            <path
-              id="Line_619"
-              d="M10.667 0L0 0"
-              className="createSvgIcon"
-              transform="translate(525.667 97.022)"
-              style={{ stroke: props.stroke }}
-            />
-            <path
-              id="Line_620"
-              d="M0 10.667L0 0"
-              className="createSvgIcon"
-              transform="translate(531 91.688)"
-              style={{ stroke: props.stroke }}
-            />
-          </g>
-        </>
+        <path
+          d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"
+          style={{ fill: props.fill }}
+        />
       );
-
     default:
       return <path />;
   }
